7465f943d6d2c8aca7e07c1457d6cfbf3b01b6c5
Sung Kang <sk5156@gmail.com>
2026-02-18 22:45:55 -0600

    add vial files

---
+++ keyboards/cipulot/ec_prox/ansi_iso/keymaps/vial/config.h
@@ -0,0 +1,23 @@
+/* Copyright 2023 Cipulot
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE2, 0x86, 0xCB, 0xAF, 0x97, 0x23, 0xD0, 0xE5}
+
+// Unlock Vial with Escape + Enter
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+#define VIAL_UNLOCK_COMBO_COLS {0, 13}
